7679e9a09407b63946f4d43ea17858806a39d4d9
Marco Antonio Hernandez Alba <marco@grupohernandezalba.com>
2025-03-15 13:16:49 -0600

    se agrego cors

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import conexion from './conexion.js';
 import productosRoutes from './routes/productosRoutes.js';
 
 const app = express();
+
+app.use(cors());
+
+app.use(cors({
+    origin: 'https://zebromex.com'
+  }));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
