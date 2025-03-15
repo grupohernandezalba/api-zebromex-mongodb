0108dd15ecc2fa3048dc9657a76e6ed4d5c30297
Marco Antonio Hernandez Alba <marco@grupohernandezalba.com>
2025-03-14 22:02:11 -0600

    cors

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 import express from 'express';
-const app = express();
-import productosRoutes from './routes/productosRoutes.js';
-
-import documentacionRoutes from './routes/documentacionRoutes.js';
- 
-app.set('view engine', 'ejs');
 import ejs from 'ejs';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import conexion from './conexion.js';
+import productosRoutes from './routes/productosRoutes.js';
+import documentacionRoutes from './routes/documentacionRoutes.js';
+
+ 
+const app = express();
+
+app.use(cors());
+app.use(express.static('public'));
+app.set('views', './views');
+
+app.set('view engine', 'ejs');
 app.engine('ejs', ejs.renderFile);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-import mongoose from 'mongoose';
-import conexion from './conexion.js';
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 
 mongoose.connect(conexion)
     .then(() => {
@@ -26,6 +40,8 @@ app.listen(3000, () => {
     console.log('Servidor iniciado en http://localhost:3000');
 });
 
+
 app.use(productosRoutes);
 app.use(documentacionRoutes);
+
 
