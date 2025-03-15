f46b9ecdb6890bbd360bc8c8e675c266b0bd71b7
Marco Antonio Hernandez Alba <marco@grupohernandezalba.com>
2025-03-15 13:02:55 -0600

    se agrego endpoint idproducto y categoria

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -10,4 +10,26 @@ const getProductos = (req, res) => {
     });
 };
 
-export default { getProductos };
+const getProductosPorCategoria = (req, res) => {
+    console.log(req.params.idcategoria);
+    Producto.find({ categoria : req.params.idcategoria })
+    .then(productos => {
+        res.json(productos);
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    });
+};
+
+const getProducto = (req, res) => {
+    Producto.find({ _id : req.params.id })
+    .then(producto => {
+        res.json(producto);
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    });
+};
+
+
+export default { getProductos, getProductosPorCategoria, getProducto }; 
--- routes/productosRoutes.js
+++ routes/productosRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 import productosController from '../controllers/productosController.js';
 
 router.get('/productos', productosController.getProductos);
+router.get('/categoria/:idcategoria', productosController.getProductosPorCategoria);
+router.get('/:id', productosController.getProducto);
 
 export default router;
