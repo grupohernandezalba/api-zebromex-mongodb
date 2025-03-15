15ffdfce5e71e25e8f6edfa1e1d1df6a4243f046
Marco Antonio Hernandez Alba <marco@grupohernandezalba.com>
2025-03-14 18:43:46 -0600

    se quito fs y https

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 const express = require('express');
-const fs = require('fs');
-const fileUpload = require('express-fileupload');
-const https = require('https');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -11,7 +8,6 @@ require('dotenv/config');
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(fileUpload());
 
 const empresaRoute = require('./rutas/empresa');
 const menuRoute = require('./rutas/menu');
@@ -36,14 +32,5 @@ mongoose.connect(
 
 app.listen(PORT, () => {
       console.log(`Example app listening on port ${PORT}`)
-    })
+    });
 
-    /*
-https.createServer({
-      key: fs.readFileSync('../../ssl/keys/d10d9_128a3_f859e374cb453b28e7bf8d13d13db07b.key'),
-      cert: fs.readFileSync('../../ssl/certs/_wildcard__zebromex_com_d10d9_128a3_1746851711_f27676571a742a1fca274f66767a23a4.crt')
-  }, app).listen(PORT, function(){
-      console.log("My https server listening on port " + PORT + "...");
-  });
-
-  */
