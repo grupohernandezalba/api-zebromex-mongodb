14b10291a0348d9d95c328cc7d284346af12d51b
Marco Antonio Hernandez Alba <marco@grupohernandezalba.com>
2025-04-03 13:11:47 -0600

    orden de menu con sort

--- controllers/menusController.js
+++ controllers/menusController.js
@@ -1,7 +1,7 @@
 import Menu from '../models/Menu.js';
 
 const getMenus = (req, res) => {
-    Menu.find()
+    Menu.find().sort( { "int_orden_men": 1 } )
     .then(menus => {
         res.json(menus);
     })
