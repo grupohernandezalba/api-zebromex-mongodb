f30974cb5582e26a13f535a9bdeca83c98e35635
grupohernandezalba <marco@grupohernandezalba.com>
2025-11-03 13:32:01 -0600

    se agrego verbos post, put y delete en api de blog

--- controllers/blogsController.js
+++ controllers/blogsController.js
@@ -20,4 +20,37 @@ const getBlog = (req, res) => {
     });
 };
 
-export default { getBlogs, getBlog }; 
+const deleteBlog = (req, res) => {
+    Blog.deleteOne({ _id : req.params.id })
+    .then(registros => {
+        res.json(registros);
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    });
+}
+
+const createBlog = (req, res) => {
+    const datos = req.body;
+    const nuevoBlog = new Blog(datos);
+    nuevoBlog.save()
+    .then(registro => {
+        res.json(registro);
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    });
+};
+
+const updateBlog = (req, res) => {
+    const datos = req.body;
+    Blog.updateOne({ _id : req.params.id }, datos)
+    .then(registro => {
+        res.json(registro);
+    })
+    .catch(err => {
+        res.status(400).send(err);
+    });
+};
+
+export default { getBlogs, getBlog, deleteBlog, createBlog, updateBlog }; 
--- routes/blogsRoutes.js
+++ routes/blogsRoutes.js
@@ -4,5 +4,9 @@ import blogsController from '../controllers/blogsController.js';
 
 router.get('/blogs', blogsController.getBlogs);
 router.get('/blog/:id', blogsController.getBlog);
+router.delete('/blog/:id', blogsController.deleteBlog);
+router.post('/blog', blogsController.createBlog);
+router.put('/blog/:id', blogsController.updateBlog);
+
 
 export default router;
